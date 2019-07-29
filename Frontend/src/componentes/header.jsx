c60b315560a0a3bf4f190cd855e76058f107e51f
car <murillo.carlos@correounivalle.edu.co>
2019-07-29 17:54:00 -0500

    1.0.1

--- Frontend/src/componentes/header.jsx
+++ Frontend/src/componentes/header.jsx
@@ -3,7 +3,93 @@ import React from 'react'
 //Header simple
 const Header = (props) => {
     return(
-        <header>Este es el hijueputa header</header>
+        <div id="header">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-3">
+                    <div className="header-logo">
+                        <a href="#" className="logo">
+                            <img src="./img/logo.png" alt=""/>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="col-md-6">
+                    <div className="header-search">
+                        <form>
+                            <select className="input-select">
+                                <option value="0">All Categories</option>
+                                <option value="1">Category 01</option>
+                                <option value="1">Category 02</option>
+                            </select>
+                            <input className="input" placeholder="Search here"/>
+                            <button className="search-btn">Search</button>
+                        </form>
+                    </div>
+                </div>
+
+                <div className="col-md-3 clearfix">
+                    <div className="header-ctn">
+                        <div>
+                            <a href="#">
+                                <i className="fa fa-heart-o"></i>
+                                <span>Your Wishlist</span>
+                                <div className="qty">2</div>
+                            </a>
+                        </div>
+
+                        <div className="dropdown">
+                            <a className="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+                                <i className="fa fa-shopping-cart"></i>
+                                <span>Your Cart</span>
+                                <div className="qty">3</div>
+                            </a>
+                            <div className="cart-dropdown">
+                                <div className="cart-list">
+                                    <div className="product-widget">
+                                        <div className="product-img">
+                                            <img src="./img/product01.png" alt=""/>
+                                        </div>
+                                        <div className="product-body">
+                                            <h3 className="product-name"><a href="#">product name goes here</a></h3>
+                                            <h4 className="product-price"><span className="qty">1x</span>$980.00</h4>
+                                        </div>
+                                        <button className="delete"><i className="fa fa-close"></i></button>
+                                    </div>
+
+                                    <div className="product-widget">
+                                        <div className="product-img">
+                                            <img src="./img/product02.png" alt=""/>
+                                        </div>
+                                        <div className="product-body">
+                                            <h3 className="product-name"><a href="#">product name goes here</a></h3>
+                                            <h4 className="product-price"><span className="qty">3x</span>$980.00</h4>
+                                        </div>
+                                        <button className="delete"><i className="fa fa-close"></i></button>
+                                    </div>
+                                </div>
+                                <div className="cart-summary">
+                                    <small>3 Item(s) selected</small>
+                                    <h5>SUBTOTAL: $2940.00</h5>
+                                </div>
+                                <div className="cart-btns">
+                                    <a href="#">View Cart</a>
+                                    <a href="#">Checkout  <i className="fa fa-arrow-circle-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="menu-toggle">
+                            <a href="#">
+                                <i className="fa fa-bars"></i>
+                                <span>Menu</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     )
 }
 export default Header;
--- Frontend/src/componentes/main.jsx
+++ Frontend/src/componentes/main.jsx
@@ -1,88 +1,30 @@
 /* Por el momento solo se tiene el login como ejemplo */
 import React, { Component } from 'react'
-import Form from 'react-bootstrap/Form'
-import { Button } from 'react-bootstrap'
-import axios from 'axios'
-
+import Header from './header'
+import Footer from './footer'
+import Producto from './producto'
 /*
 Los componentes con estado deben ser clases
 vea un ejemplo de componente sin estado en el
 footer o header
 */
 
-class Login extends Component {
 
-  //Iniciamos los atributos
-  constructor(props) {
-    super(props)
-    this.state = {
-      password: "",
-      nick: ""
-    };
-    //Esto es necesario para la identificacion de funciones
-    this.enviar = this.enviar.bind(this);
-    this.onChange = this.onChange.bind(this) 
-  }
-
-  //FUncion que obtiene y cambia un stributo del estado
-  onChange = input => e =>{ 
-    this.setState({ [input]: e.target.value});
-  }  
-
-  //Funcion ejemplo para hacer solicitud al servidor
-  enviar() {
-    const mensaje = {
-      password: this.state.password,
-      nick: this.state.nick
-    }
-
-    //Axios se encarga de hacer solicitudes de forma sencilla
-    axios.post('http://localhost:5000/login', mensaje)
-    .then((response) => {
-      alert(JSON.stringify(response.data))
-    })
-  }
-
-  //El metodo render es obligario para los componentes con estado
-  render() {
-
+const Main = (props) => {
     //Aqui podemos hacer validaciones para retonar diferentes cosas
 
     //El metodo retunr es necesario
     return (
+      <div>
+      <Header/>
       <main>
-        <Form className="formulario">
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Ingrese su usuario</Form.Label>
-            <Form.Control placeholder="Usuario" 
-              onChange = {this.onChange('nick')}
-            />
-            {/*<Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-            */}
-          </Form.Group>
-            <br/>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Contraseña</Form.Label>
-            <Form.Control type="password" 
-              onChange={this.onChange('password')}
-              placeholder="Password" 
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicChecbox">
-            <Form.Check custom type="checkbox" label="Recordar" />
-          </Form.Group>
-          <div className="center">
-            <Button variant="danger" onClick={this.enviar}>
-              Iniciar
-            </Button>
-          </div>
-        </Form>
+        <Producto/>
       </main>
+      <Footer/>
+      </div>
 
     );
-  }
+  
 }
 
-export default Login;
+export default Main;
