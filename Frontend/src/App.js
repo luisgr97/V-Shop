79fceed8fa6604023009e00252aa5e5ee3ad5c80
car <murillo.carlos@correounivalle.edu.co>
2019-07-29 18:09:50 -0500

    "Revert "1.0.1""
    This reverts commit 38fe4899722dc6888f4b4e98cd3042df1a006163.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import Header from './componentes/header'
 import Main from './componentes/main'
-import Footer from './componentes/footer'
+import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom'
+import LoginAdmin from './componentes/login'
 
 //En esta parte renderizamos lo prinipal
 function App() {
   return (
+   
 
     //En react, para dar estilos CSS usamos className en lugar de class
     <div className="App">
-     <Header/>
-     <Main/>
-     <Footer/>
+
+      <Router>
+        <Route exact path="/wp-admin" render={() => 
+            <LoginAdmin  
+              //url={URL}
+              //autenticado={this.state.autenticado} 
+            />}>
+          </Route>
+          <Route exact path="/" render={() => 
+            <Main  
+              //url={URL}
+              //autenticado={this.state.autenticado} 
+            />}>
+          </Route>
+        
+        </Router>
+
     </div>
   );
 }
