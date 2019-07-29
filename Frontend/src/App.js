fe7ba5bd13a2c562a88b713bebd7895066a644fa
esneidermanzano <esneidermanzano@gmail.com>
2019-07-29 18:19:13 -0500

    1.0.1
    This reverts commit c60b315560a0a3bf4f190cd855e76058f107e51f.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
+import Header from './componentes/header'
 import Main from './componentes/main'
-import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom'
-import LoginAdmin from './componentes/login'
+import Footer from './componentes/footer'
 
 //En esta parte renderizamos lo prinipal
 function App() {
   return (
-   
 
     //En react, para dar estilos CSS usamos className en lugar de class
     <div className="App">
-
-      <Router>
-        <Route exact path="/wp-admin" render={() => 
-            <LoginAdmin  
-              //url={URL}
-              //autenticado={this.state.autenticado} 
-            />}>
-          </Route>
-          <Route exact path="/" render={() => 
-            <Main  
-              //url={URL}
-              //autenticado={this.state.autenticado} 
-            />}>
-          </Route>
-        
-        </Router>
-
+     <Header/>
+     <Main/>
+     <Footer/>
     </div>
   );
 }
