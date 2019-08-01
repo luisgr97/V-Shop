895d85bb0f940567661fe810220d05ad8874e425
luisGomez231997 <37640684+luisGomez231997@users.noreply.github.com>
2019-08-01 00:50:17 -0500

    Add files via upload

---
+++ Backend/src/controllers/dao.cliente.js
@@ -0,0 +1,86 @@
+import Cliente from '../modelos/cliente'
+import Usuario from '../modelos/usuario'
+
+//Sirve para crear todos los clientes
+export async function createCliente(req, res) {
+    try {
+        const { estado, clave, nick, tarjeta, id_usuario } = req.body;
+        const cliente = await Cliente.create({ estado, clave, nick, tarjeta, id_usuario });
+        if (cliente) {
+            res.json(cliente);
+        }
+    } catch (e) {
+        res.json({ message: 'error', e });
+    }
+}
+
+//Sirve para actualizar todos los clientes
+export async function updateCliente(req, res) {
+    try {
+        const { id_cliente, estado, clave, nick, tarjeta } = req.body;
+        const cliente = await Cliente.findOne({
+            attributes: ['estado', 'clave', 'nick', 'tarjeta'],
+            where: { id_cliente: id_cliente }
+        })
+        const updateCliente = await Cliente.update({ estado, clave, nick }, {
+            where: { id_cliente: id_cliente }
+        });
+        if (updateCliente) {
+            res.json(updateCliente);
+        }
+
+    } catch (e) {
+        res.json({ message: 'error', e });
+    }
+}
+
+//Esta funcion obtiene cliente segun id
+export async function getOneCliente(req, res) {
+    const { id_cliente } = req.body;
+    const cliente = await Cliente.findOne({
+        where: {
+            id_cliente: id_cliente
+        }
+    });
+    //Si no encontro retorna falso
+    if (cliente) {
+        res.send(cliente)
+    } else {
+        res.send({ encontrado: 'false' })
+    }
+}
+
+//Sirve para obtener todos los clientes
+export async function listClientes(req, res) {
+    console.log(req.body)
+    const cliente = await Cliente.findAll();
+    res.send(cliente)
+}
+
+//Sirve para eliminar todos los gerentes
+export async function deleteCliente(req, res) {
+    try {
+        const { id_cliente } = req.body;
+        const cliente = await Cliente.destroy({ where: { id_cliente: id_cliente } })
+        res.json({ message: 'eliminado' });
+    } catch (e) {
+        res.json({ message: 'error', e });
+    }
+}
+
+//Esta funcion obtiene admin segun clave y nick
+export async function logCliente(req, res) {
+    const { nick, clave } = req.body;
+    const cliente = await Cliente.findOne({
+        where: {
+            nick: nick,
+            clave: clave
+        }
+    });
+    //Si no encontro retorna falso
+    if (cliente) {
+        res.send(admin)
+    } else {
+        res.send({ encontrado: 'false' })
+    }
+}
---
+++ Backend/src/controllers/dao.usuario.js
@@ -0,0 +1,130 @@
+import Administrador from '../modelos/administrador'
+import Usuario from '../modelos/usuario';
+
+//Sirve para crear todos los usuarios
+export async function createUsuario(req, res) {
+    // try {
+    const {
+        //   id_usuario,
+        documento,
+        numero,
+        nombres,
+        apellidos,
+        telefono,
+        direccion,
+        cumpleanos,
+        nacimiento,
+        correo
+    } = req.body;
+    const usuario = await Usuario.create({
+        //  id_usuario,
+        documento,
+        numero,
+        nombres,
+        apellidos,
+        telefono,
+        direccion,
+        cumpleanos,
+        nacimiento,
+        correo
+    });
+    if (usuario) {
+        res.json(usuario);
+    }
+    //   } catch (e) {
+    //        res.json({ message: 'error' });
+    //    }
+}
+
+//Sirve para borras  los usuarios
+export async function deleteUsuario(req, res) {
+    try {
+        const { id } = req.body;
+        const usuario = await Usuario.destroy({ where: { id: id } })
+        res.json({ message: 'eliminado' });
+    } catch (e) {
+        res.json({ message: 'error', e });
+    }
+}
+
+//Esta funcion obtiene admin segun id
+export async function getOneUsuario(req, res) {
+    const { id } = req.body;
+    const usuario = await Usuario.findOne({
+        attributes: ['documento', 'numero', 'nombres', 'apellidos', 'telefono', 'direccion', 'cumpleanos', 'nacimiento', 'correo'],
+        where: {
+            id: id
+        }
+    });
+    //Si no encontro retorna falso
+    if (usuario) {
+        res.json(usuario);
+    } else {
+        res.send({ encontrado: 'false' })
+    }
+}
+
+export async function updateUsuario(req, res) {
+    try {
+        const {
+            id,
+            documento,
+            numero,
+            nombres,
+            apellidos,
+            telefono,
+            direccion,
+            cumpleanos,
+            nacimiento,
+            correo
+        } = req.body;
+        const usuario = await Usuario.findOne({
+            attributes: ['documento', 'numero', 'nombres', 'apellidos', 'telefono', 'direccion', 'cumpleanos', 'nacimiento', 'correo'],
+            where: { id: id }
+        })
+        const updateUsuario = await Usuario.update({
+            documento,
+            numero,
+            nombres,
+            apellidos,
+            telefono,
+            direccion,
+            cumpleanos,
+            nacimiento,
+            correo
+        }, {
+            where: { id: id }
+        });
+        if (updateUsuario) {
+            res.json(updateUsuario);
+        }
+
+    } catch (e) {
+        res.json({ message: 'error', e });
+    }
+}
+
+//Sirve para obtener todos los admins
+export async function listUsuarios(req, res) {
+    const usuario = await Usuario.findAll({
+        attributes: ['documento', 'numero', 'nombres', 'apellidos', 'telefono', 'direccion', 'cumpleanos', 'nacimiento', 'correo']
+    });
+    res.send(usuario)
+}
+
+//Esta funcion obtiene admin segun clave y nick
+export async function logUsuario(req, res) {
+    const { nick, clave } = req.body;
+    const usuario = await Usuario.findOne({
+        where: {
+            nick: nick,
+            clave: clave
+        }
+    });
+    //Si no encontro retorna falso
+    if (usuario) {
+        res.send(usuario)
+    } else {
+        res.send({ encontrado: 'false' })
+    }
+}
