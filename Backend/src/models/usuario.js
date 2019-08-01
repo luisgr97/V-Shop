21c40c0482243b6165b129d095b524f85c29d1bc
luisGomez231997 <37640684+luisGomez231997@users.noreply.github.com>
2019-08-01 00:49:10 -0500

    Add files via upload

---
+++ Backend/src/models/usuario.js
@@ -0,0 +1,43 @@
+import Sequelize from 'sequelize'
+import { sequelize } from '../db'
+
+//Asi se especifica un modelo de tabla usando el ORM
+const Usuario = sequelize.define('usuario', {
+    id_usuario: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    documento: {
+        type: Sequelize.STRING(2)
+    },
+    numero: {
+        type: Sequelize.STRING(10)
+    },
+    nombres: {
+        type: Sequelize.STRING(50)
+    },
+    apellidos: {
+        type: Sequelize.STRING(50)
+    },
+    telefono: {
+        type: Sequelize.STRING(10)
+    },
+    direccion: {
+        type: Sequelize.STRING(50)
+    },
+    cumpleanos: {
+        type: Sequelize.DATEONLY
+    },
+    nacimiento: {
+        type: Sequelize.DATEONLY
+    },
+    correo: {
+        type: Sequelize.STRING(50)
+    }
+}, {
+    freezeTableName: true,
+    timestamps: false
+})
+
+
+export default Usuario;
